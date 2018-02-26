846baf74c20f19fb14b7cc6807b3d3b0c20922a5
Leo Q <leoq91@gmail.com>
2018-02-25 22:40:52 -0800

    us map and eng income above median income

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import _ from 'lodash';
+
+import './App.css';
+import Preloader from './components/Preloader';
+import { loadAllData } from './DataHandling';
+
+import CountyMap from './components/CountyMap';
+import Histogram from './components/Histogram';
+
+class App extends Component {
+    state = {
+        techSalaries: [],
+        countyNames: [],
+        medianIncomes: [],
+        usTopoJson: {},
+    };
+
+    componentWillMount() {
+        loadAllData(data => this.setState(data));
+    }
+
+    countyValue(county, techSalariesMap) {
+        const medianHousehold = this.state.medianIncomes[county.id],
+            salaries = techSalariesMap[county.name];
+
+        if (!medianHousehold || !salaries) {
+            return null;
+        }
+
+        const median = d3.median(salaries, d => d.base_salary);
+
+        return {
+            countyID: county.id,
+            value: median - medianHousehold.medianIncome,
+        };
+    }
+
+    render() {
+        if (this.state.techSalaries.length < 1) {
+            return <Preloader />;
+        }
+
+        const filteredSalaries = this.state.techSalaries,
+            filteredSalariesMap = _.groupBy(filteredSalaries, 'countyID'),
+            countyValues = this.state.countyNames
+                .map(county => this.countyValue(county, filteredSalariesMap))
+                .filter(d => !_.isNull(d));
+
+        let zoom = null;
+        return (
+            <div className="App container">
+                <svg width="1100" height="500">
+                    <CountyMap
+                        usTopoJson={this.state.usTopoJson}
+                        USstateNames={this.state.USstateNames}
+                        values={countyValues}
+                        x={0}
+                        y={0}
+                        width={500}
+                        height={500}
+                        zoom={zoom}
+                    />
+                    <Histogram
+                        bins={10}
+                        width={500}
+                        height={500}
+                        x="500"
+                        y="10"
+                        data={filteredSalaries}
+                        axisMargin={83}
+                        bottomMargin={5}
+                        value={d => d.base_salary}
+                    />
+                </svg>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Histogram/Axis.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+const blackBox = D3render => {
+    return class Blackbox extends Component {
+        componentDidMount() {
+            D3render.call(this);
+        }
+        componentDidUpdate() {
+            D3render.call(this);
+        }
+
+        render() {
+            const { x, y } = this.props;
+            return <g transform={`translate(${x}, ${y})`} ref="anchor" />;
+        }
+    };
+};
+
+const Axis = blackBox(function() {
+    const axis = d3
+        .axisLeft()
+        .tickFormat(d => `${d3.format('.2s')(d)}`)
+        .scale(this.props.scale)
+        .ticks(this.props.data.length);
+    d3.select(this.refs.anchor).call(axis);
+});
+
+export default Axis;
